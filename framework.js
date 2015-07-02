b1aafcc35c181cf4cd31db667fa26238ed945628
klasiik <klasiik@go2.pl>
2015-07-02 23:00:57 +0200

    start

---
+++ framework.js
@@ -0,0 +1,27 @@
+var framework = function(){
+	return{
+		create: function(element){
+			var elo = document.createElement(element);
+			return elo;
+		},
+		append: function(par, element){
+			par.appendChild(element);
+		},
+		tag: function(element){
+			var chuj = document.getElementsByTagName(element);
+			return chuj;
+		},
+		attr: function(object, attributes){
+			for (var property in attributes) {
+				object[property] = attributes[property];
+			};
+		}
+	};
+}();
+
+var addNewItemUpcoming = function(){
+			var newItemDiv = document.createElement("li");
+			listUpcoming.appendChild(newItemDiv);
+			newItemDiv.innerHTML = newItemInput.value;
+		};
+
---
+++ script.js
@@ -0,0 +1,39 @@
+var body = framework.tag("body")[0];
+framework.attr(body, {
+	background: "gfx/back.jpg"
+})
+
+var container = framework.create("div");
+framework.append(body, container);
+
+//new item addition 
+/*var newItemLabel = framework.create("label");
+framework.append(container, newItemLabel);
+framework.attr(newItemLabel, {
+	for: content,
+	innerHTML: "Add new thing to do: "
+})*/
+
+var newItemInput = framework.create("input");
+framework.append(container, newItemInput);
+framework.attr(newItemInput, {
+	id: "content",
+	name: "content",
+	placeholder: "Type new item to add"
+})
+
+var newItemSubmit = framework.create("input");
+framework.append(container, newItemSubmit);
+framework.attr(newItemSubmit, {
+	type: "submit",
+	value: "Add"
+});
+
+var listUpcomingDiv = framework.create("div");
+framework.append(container, listUpcomingDiv);
+listUpcomingDiv.className = "upcoming";
+var listUpcoming = framework.create("ul");
+framework.append(listUpcomingDiv, listUpcoming);
+
+
+newItemSubmit.addEventListener("click", addNewItemUpcoming, false);
