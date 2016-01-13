83a424cdda17c2d07c14dd9b4097bc502a516e40
klasiik <klasiik@go2.pl>
2016-01-13 03:23:20 +0100

    multiple lists

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+/*ADD NEW LIST*/
+var listAmount = 0;
+
+var addNewList = function() {
+	listAmount = listAmount + 1;
+
+	var container = document.getElementById("container");
+
+	var listContainer = document.createElement("div");
+	container.appendChild(listContainer);
+	listContainer.id = "list" + listAmount;
+
+	var addDiv = document.createElement("div");
+	container.appendChild(addDiv);
+	addDiv.id = "add";
+
+ 	var inputText = document.createElement("input");
+	addDiv.appendChild(inputText);
+	inputText.type = "text";
+	inputText.placeholder = "Add item...";
+	inputText.name = "add-item";
+	inputText.id = "add-text";
+
+	var inputButton = document.createElement("input");
+	addDiv.appendChild(inputButton);
+	inputButton.type = "button";
+	inputButton.value = "ADD ITEM";
+	inputButton.name = "add-item-button";
+	inputButton.id = "add-button";
+
+	var listDiv = document.createElement("div");
+	container.appendChild(listDiv);
+	listDiv.id = "list";
+
+	var listUl = document.createElement("ul");
+	listDiv.appendChild(listUl);
+	listUl.id = "list-ul";
+};
+
+var newListButton = document.getElementById("add-item-button");
+newListButton.addEventListener("click", addNewList);
+
+/* NEW LIST ITEM*/
+
 var newListItem = function() {
 	var uList = document.getElementById("list-ul");
 	var newItemInput = document.getElementById("add-text");
@@ -5,10 +49,27 @@ var newListItem = function() {
 	var newItemDiv = document.createElement("li");
 	uList.appendChild(newItemDiv);
 	newItemDiv.innerHTML = newItemInput.value;
+
+	var newItemDel = document.createElement("div");
+	newItemDiv.appendChild(newItemDel);
+	newItemDel.innerHTML = "Delete";
+
+	newItemDel.addEventListener("click", deleteListItem);
+
+	var newItemDone = document.createElement("div");
+	newItemDiv.appendChild(newItemDone);
+	newItemDone.innerHTML = "Done";
 };
 
 var addButton = document.getElementById("add-button");
 addButton.addEventListener("click", newListItem);
+
+/*DELETE LIST ITEM */
+var deleteListItem = function() {
+	this.parentNode.parentNode.removeChild(this.parentNode);
+};
+
+
 
 /*var body = framework.tag("body")[0];
 framework.attr(body, {
